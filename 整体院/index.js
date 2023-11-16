70961b31c3e5fb3a3cabed2088d3e18b4239d527
Shiho-fuji <143941894+Shiho-fuji@users.noreply.github.com>
2023-11-16 23:21:57 +0900

    seitai

---
+++ 整体院/index.js
@@ -0,0 +1,24 @@
+const mySwiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 2500,
+  },
+ 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
