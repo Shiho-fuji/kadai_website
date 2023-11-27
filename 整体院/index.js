e10940052ad61c2432d9155cb9bdbcc545138e1f
Shiho-fuji <143941894+Shiho-fuji@users.noreply.github.com>
2023-11-28 00:38:38 +0900

    update1127_02

--- 整体院/index.js
+++ 整体院/index.js
@@ -46,3 +46,24 @@ window.addEventListener("scroll", function () {
   // 100px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
   header.classList.toggle("scroll-nav", window.scrollY > 100);
 });
+
+
+
+window.onload=function(){
+  let Animation = function() {
+  //アイコン位置取得
+  let pageTop =  document.getElementById('page_top');
+
+  //要素の位置座標を取得
+  let rect = pageTop.getBoundingClientRect();
+  //topからの距離
+  let scrollTop = rect.top + window.pageYOffset;
+
+  if(scrollTop > 1000){
+    pageTop.classList.add('show');
+   }  else {
+    pageTop.classList.remove('show');
+   }
+ }
+    window.addEventListener('scroll', Animation);
+}
