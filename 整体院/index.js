f4a7b4f71bc3d664495483a7dfe330a0a8a68f88
Shiho-fuji <143941894+Shiho-fuji@users.noreply.github.com>
2023-11-27 23:22:47 +0900

    update1127

--- 整体院/index.js
+++ 整体院/index.js
@@ -10,6 +10,7 @@ const mySwiper = new Swiper('.swiper', {
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    type: 'progressbar'
   },
  
   // Navigation arrows
@@ -22,4 +23,26 @@ const mySwiper = new Swiper('.swiper', {
   scrollbar: {
     el: '.swiper-scrollbar',
   },
+});
+
+window.onload = function () {
+  var nav = document.getElementById('nav-wrapper');
+  var hamburger = document.getElementById('js-hamburger');
+  var blackBg = document.getElementById('js-black-bg');
+
+  hamburger.addEventListener('click', function () {
+      nav.classList.toggle('open');
+  });
+  blackBg.addEventListener('click', function () {
+      nav.classList.remove('open');
+  });
+};
+
+
+
+window.addEventListener("scroll", function () {
+  // ヘッダーを変数の中に格納する
+  const header = document.querySelector("header");
+  // 100px以上スクロールしたらヘッダーに「scroll-nav」クラスをつける
+  header.classList.toggle("scroll-nav", window.scrollY > 100);
 });
